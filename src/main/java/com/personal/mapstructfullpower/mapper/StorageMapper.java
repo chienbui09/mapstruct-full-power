623c6bed7b8f3a1f2e3a72acda0813dbac3f60db
chienbui09 <buiducchientlf2209@gmail.com>
2023-07-26 01:26:05 +0700

    feat: test mapstruct for 2 entities which has the same field

--- src/main/java/com/personal/mapstructfullpower/mapper/StorageMapper.java
+++ src/main/java/com/personal/mapstructfullpower/mapper/StorageMapper.java
@@ -1,6 +1,7 @@
 package com.personal.mapstructfullpower.mapper;
 
 import com.personal.mapstructfullpower.dto.ShelveDto;
+import com.personal.mapstructfullpower.dto.ShelveDto2;
 import com.personal.mapstructfullpower.entity.ShelveEntity;
 import com.personal.mapstructfullpower.mapannotation.ToEntity;
 import org.mapstruct.Mapper;
@@ -12,5 +13,9 @@ public abstract class StorageMapper {
     @ToEntity
     @Mapping(target = "weighLimit",source = "maxWeight")
     public abstract ShelveEntity map(ShelveDto dto);
+    @Mapping(target = "weighLimit",source = "dto.maxWeight")
+    @Mapping(target = "name",source = "dto2.groupName")
+    public abstract ShelveEntity map2Dto(ShelveDto dto, ShelveDto2 dto2);
+
 
 }
